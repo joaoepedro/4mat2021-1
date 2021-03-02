7832dbc7cada8a0d3566155888d697fbda76f4d3
joaoepedro <opedroejoao@gmail.com>
2021-03-02 22:42:49 +0000

    (23/02) Término da revisão de JS

--- revisao-js/metodos-vetor.js
+++ revisao-js/metodos-vetor.js
@@ -44,3 +44,83 @@ const pets = [
 ]
 
 // ALT + SHIFT + F
+
+// find(): retorna a primeira ocorrência que corresponda ao critério informado
+// retorna undefined quando nao encontra nenhum elemento coincidente
+console.log(pets.find(elemento => elemento.peso > 10))
+console.log(pets.find(pet => pet.idade < 4))
+console.log(pets.find(pet => pet.especie === 'gato' && pet.sexo == 'M'))
+console.log(pets.find(pet => pet.especie === 'calopsita'))
+
+console.log('--------------------------------------------------------------------')
+
+// some(): retorna true caso haja algum elemento que corresponda ao criterio ou false caso contrario
+console.log(pets.some(pet => pet.nome === 'Cerberus'))
+console.log(pets.some(pet => pets.especie === 'tartaruga' && pets.sexo === 'F'))
+
+console.log('--------------------------------------------------------------------')
+
+// every(): retorna true caso todos os elementos correspondam ao criterio ou false caso contrario
+console.log(pets.every(pet => pet.especie === 'cão'))
+console.log(pets.every(pet => pet.peso >= 1))
+
+console.log('--------------------------------------------------------------------')
+
+// filter(): retorna um novo vetor contendo apenas os elementos que correspondem ao critério
+console.log(pets.filter(x => x.especie === 'gato'))
+console.log(pets.filter(x => x.idade > 20))
+console.log(pets.filter(x => x.sexo === 'F' && x.especie === 'papagaio'))
+
+console.log('--------------------------------------------------------------------')
+
+// map(): cria um novo vetor com o mesmo número de elementos do vetor original,
+// correspondendo a algum tipo de transformação nos elementos originais
+
+// Novo vetor contendo apenas os nomes dos pets, em maiúsculas
+console.log(pets.map(e => e.nome.toUpperCase()))
+
+const numeros = [2, 6, -3, 9, 5, -7, 1, 4]
+console.log(numeros)
+
+// Criando um novo vetor em que cada elemento corresponde ao elemento do vetor original elevado ao quadrado
+console.log(numeros.map(x => x ** 2))
+
+console.log('--------------------------------------------------------------------')
+// reduce(): reduz um vetor de valores a apenas valor - basicamente um loop
+// parametros:
+// acumulador: resultado parcial até o momento
+// valor atual: o resultado correspondente ao elemento atual, que integrará o acumulador
+
+// Soma
+console.log(numeros.reduce((acu, val) => acu + val))
+
+// Multiplicação
+console.log(numeros.reduce((acu, val) => acu * val))
+
+// Concatenação
+let p1 = 'Bom '
+let p3 = 'galera!'
+console.log([p1, 'dia ', p3].reduce((a, v) => a + v))
+
+/*************************************
+ * Somar o peso de todos os pets
+*/
+
+// Abordagem 1: criando um vetor auxiliar contendo apenas os pesos dos pets
+let pesos = pets.map(pet => pet.peso)
+console.log(pesos)
+console.log(pesos.reduce((a, v) => a + v))
+
+// Abordagem 2: fazendo map() + reduce() em uma linha só
+console.log(pets.map(pet => pet.peso).reduce((a, v) => a + v))
+
+// Abordagem 3: reduce com parametros extras
+
+// 2º parametro do reduce: valor inicial do acumulador
+console.log(pets.reduce((acu, val) => acu + val.peso, 0))
+
+console.log('--------------------------------------------------------------------')
+
+// includes(): retorna true caso o vetor inclua o valor informado
+console.log(numeros.includes(6))
+console.log(numeros.includes(11))
--- revisao-js/parametros-predefinidos.js
+++ revisao-js/parametros-predefinidos.js
@@ -13,3 +13,6 @@ function calculaArea(base, altura, forma = 'Q'){
 console.log(calculaArea(2,3))
 console.log(calculaArea(5,3,'T'))
 console.log(calculaArea(4.5,4.5,'E'))
+
+// 1) Parâmetros opicionais devem vir após os obrigatórios
+// 2) pode haver mais de um parametro opicional
